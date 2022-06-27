9f7c61ce0b2be63df58c3eac155654a338b87476
🔥🐻 <firebear.dev@gmail.com>
2022-06-27 12:15:50 +0200

    ScriptableData: now is a class with a method for set the json and a proprerty for reference the scriptable object

--- Assets/Saving System/Scripts/Classes/ScriptableData.cs
+++ Assets/Saving System/Scripts/Classes/ScriptableData.cs
@@ -1,20 +1,25 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
-public struct ScriptableData 
+public class ScriptableData 
 {
     [SerializeField] private ScriptableObject _scriptableObject;
-    [SerializeField] private string _json;
+    [SerializeField] [TextArea] private string _json;
 
+    public ScriptableObject scriptableObject => _scriptableObject;
     public string json => _json;
+
 
     public ScriptableData(ScriptableObject scriptableObject)
     {
         _scriptableObject = scriptableObject;
         _json = "";
+    }
+
+    public void SetJson(string json)
+    {
+        _json = json;
     }
 
     public void Serialize()
